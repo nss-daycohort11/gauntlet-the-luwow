7a322e09f8f60e18df9b5fb0b8c79e0b35db241d
jacobpaine <painejacob@gmail.com>
2015-10-26 15:36:46 -0500

    User Profile

--- javascripts/userprofile.js
+++ javascripts/userprofile.js
@@ -29,31 +29,35 @@ var myUser = new User();
 $("#name-submit").click(function(){
 
 myUser.playerName = $("#player-name").val();
-console.log("Looking for myUser.class", myUser);
 
-
-console.log("uer profile object within click function:", myUser);
+	//User Profile
+	$("#user-profile").append("<p>" + $("#player-name").val() + "</p>");
 
 });
 
+
+//Listening for class and populating myUser
 $(".class__link").click(function(e){
 
 	var Pathname = e.currentTarget.childNodes[3].innerHTML;
 	myUser.class = new classIndex[Pathname]();
 	console.log("what's the class:", myUser.class);
 
+	//User Profile
+	$("#user-profile").append("<p>" + Pathname + "</p>");
 
 });
 
+//Listening for weapon and populating myUser
 $(".weapon__link").click(function(e){
 
 	var Pathname = e.currentTarget.childNodes[3].innerHTML;
 	myUser.weapon = new weaponIndex[Pathname]();
 	console.log("what's the weapon:", myUser.weapon);
 
+	$("#user-profile").append("<p>" + Pathname + "</p>");
 
 });
-
 
 
 });
