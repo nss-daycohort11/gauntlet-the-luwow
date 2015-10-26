682178c5d35a949c94c8b4c95232c046046ad5c1
jacobpaine <painejacob@gmail.com>
2015-10-26 14:46:58 -0500

    Classes added.

--- javascripts/userprofile.js
+++ javascripts/userprofile.js
@@ -1,16 +1,46 @@
 $(document).ready(function() {
 
+var classIndex = {
+	"warrior" : Warrior,
+	"valkyrie" : Valkyrie,
+	"berserker": Berserker,
+	"monk": Monk,
+	"wizard": Wizard,
+	"sorcerer": Sorcerer,
+	"conjurer": Conjurer,
+	"thief": Thief,
+	"ninja": Ninja,
+	"assassin": Assassin
+	};
 
-var userProfile = function(){
-}
 
-userProfile.prototype = new Human();
+var User = function() {
 
+};
+User.prototype = new Human();
+// Makes new Obj for user
+var myUser = new User();
 $("#name-submit").click(function(){
-console.log("User Name", $("#player-name").val());
+
+myUser.playerName = $("#player-name").val();
+console.log("Looking for myUser.class", myUser);
+
+
+console.log("uer profile object within click function:", myUser);
+
 });
 
+$(".class__link").click(function(e){
 
+	var Pathname = e.currentTarget.childNodes[3].innerHTML;
+	console.log(Pathname, "nth Child");
+
+	myUser.class = new classIndex[Pathname]();
+
+	console.log("what's the class:", myUser.class);
+
+
+});
 
 
 });
