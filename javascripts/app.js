9fd7487cdcf096a66172be2bb65bce6691ac1363
jacobpaine <painejacob@gmail.com>
2015-10-27 11:41:03 -0500

    Fight/victory determined in console.log

--- javascripts/app.js
+++ javascripts/app.js
@@ -61,14 +61,158 @@ $(document).ready(function() {
     $(".card").hide();
     $("." + previousCard).show();
   });
+//
+// create myOrc and myUser (userprofile)
+//
+var classIndex = {
+  "warrior" : Warrior,
+  "valkyrie" : Valkyrie,
+  "berserker": Berserker,
+  "monk": Monk,
+  "wizard": Wizard,
+  "sorcerer": Sorcerer,
+  "conjurer": Conjurer,
+  "thief": Thief,
+  "ninja": Ninja,
+  "assassin": Assassin
+};
+
+var weaponIndex = {
+  "dagger" : Dagger,
+  "broadsword" : BroadSword,
+  "waraxe": WarAxe
+};
+
+
+var User = function() {
+
+};
+User.prototype = new Human();
+// Makes new Obj for user
+var myUser = new User();
+$("#name-submit").click(function(){
+
+myUser.playerName = $("#player-name").val();
+
+  //User Profile
+  $("#user-name").html($("#player-name").val());
+
+});
+
+
+//Listening for class and populating myUser
+$(".class__link").click(function(e){
+
+  var Pathname = e.currentTarget.childNodes[3].innerHTML;
+  myUser.class = new classIndex[Pathname]();
+
+  //User Profile
+  $("#current-class").html(Pathname);
+
+
+});
+
+//Listening for weapon and populating myUser
+$(".weapon__link").click(function(e){
+
+  var Pathname = e.currentTarget.childNodes[3].innerHTML;
+  myUser.weapon = new weaponIndex[Pathname]();
+
+  //User Profile
+  $("#current-weapon").html("<p>" + myUser.weapon + "</p>");
+
+});
+
+//
+// myOrc profile
+//
+
+
+
+var Orc = function() {
+  this.health = this.health + 20;
+  this.species = "Orc";
+  this.allowedClasses = ["Warrior", "Berserker", "Shaman"];
+  this.allowedWeapons = ["Dagger", "BroadSword", "WarAxe"];
+
+  this.generateClass = function() {
+    // Get a random index from the allowed classes array
+    var random = Math.round(Math.random() * (this.allowedClasses.length - 1));
+
+    // Get the string at the index
+    var randomClass = this.allowedClasses[random];
+
+    // Composes the corresponding player class into the player object
+    this.class = new window[randomClass]();
+    return this.class;
+  }
+
+ this.setWeapon = function() {
+    // Get a random index from the allowed classes array
+    var random = Math.round(Math.random() * (this.allowedWeapons.length - 1));
+
+    // Get the string at the index
+    var randomWeapon = this.allowedWeapons[random];
+
+    // Composes the corresponding player class into the player object
+    this.weapon = new window[randomWeapon]();
+    return this.weapon;
+  }
+
+
+};
+
+Orc.prototype = new Monster();
+
+  var myOrc = new Orc();
+  var orcClass = myOrc.generateClass();
+  var orcWeapon = myOrc.setWeapon();
+// new BroadSword()
+
+  $("#monster-name").html("The Orc");
+  $("#monster-class").html(orcClass.name);
+  $("#monster-weapon").html(orcWeapon.name);
+
+console.log("info on orc", myOrc, orcClass, orcWeapon);
+
+// User and Enemy stats load on enter.battlefield button
+
+// User hits attack button
+//    my User attacks
+
+var attack = function () {
+/*  console.log("userAttack stuff", myOrc, myUser);
+  console.log("userAttack specifics", myUser.health, myUser.strength, myUser.weapon.damage);
+  console.log("myOrc specifics", myOrc.health, myOrc.strength, myOrc.weapon.damage);*/
+  
+  myOrc.health = myOrc.health - ((myUser.strength/10) + myUser.weapon.damage);
+  myUser.health = myUser.health - ((myOrc.strength/10) + myOrc.weapon.damage);
+  console.log("afterfight", myOrc.health, "afteruser", myUser.health);
+    if(myOrc.health <=0) {
+      console.log("Orc dies.");
+    }
+    if (myUser.health <=0) {
+      console.log("You enter the happy nothing.");
+    }
+
+  // myOrc.health = myOrc.health - (myUser.strength/10) + myUser.weapon.damage;
+};
+
+
+$("#attack").click(attack);
+  
+
+
+
+//    myOrc attacks
 
 
 
 // Create fight
 // Fight resolution
 // How do the childNodes work, why 3?
-// UserProfiles shouldn't move on without selecting a class, weapon, etc
-
+// UserProfiles shouldn't progress to next card without selecting a class, weapon, etc
+// On attack - using userAttack() and monsterAttack() should use the same function (using this.damage etc)?
 
 
 
